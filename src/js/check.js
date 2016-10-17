2141e5a49e68f5c10c7e41113dc95d41feab7421
Юлия Смирнова <yulsm11@yandex.ru>
2016-10-17 22:56:04 +0300

    Начинаем

--- src/js/check.js
+++ src/js/check.js
@@ -16,13 +16,13 @@ function getMessage(a, b) {
                     for (var i = 0; i < a.length; i++) { 
                     artifactsSquare = artifactsSquare + a[i] * b[i];
                     }      
-                    return "Общая площадь артефактов сжатия: " + artifactsSquare + " пикселей";
+                    return "Общая площадь артефактов сжатия: " + artifactsSquare + "  пикселей";
                 }   
                     else if (Array.isArray(a)) {           
                         var amountOfRedPoints = a.reduce(function(sum, current) {
                         return sum + current;
                         });   
-                        return  "Количество красных точек во всех строчках изображения: " +  amountOfRedPoints;
+                        return  "Количество красных точек во всех строчках изображения:" +  amountOfRedPoints;
                     }  
                         else {
                         return "Переданы некорректные данные";
